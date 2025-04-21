490681ba9b590ddced9d848db69943af87504ebf
Soyas Limbu <soyaslimbu07@gmail.com>
2025-04-22 09:32:07 +1000

    updated google analytics tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,8 +5,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../../public/assets/scss/master.scss";
 
 import { config } from "@fortawesome/fontawesome-svg-core";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag?.('config', 'G-S8R9Z6B5ZP', {
+        page_path: url,
+      });
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => router.events.off('routeChangeComplete', handleRouteChange);
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
